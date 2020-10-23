3037e19b2fd131e868562ce3e12172afbae3b458
Romano Keser <romanokeser@gmail.com>
2020-10-23 13:51:08 +0200

    Add files via upload

---
+++ MouseTrackingWPF/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace MouseTrackingWPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.recordButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.logInTab = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.userNameTexBox = new System.Windows.Forms.TextBox();
+            this.tabPageRecord = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.loadRecordButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPageContacts = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.logInTab.SuspendLayout();
+            this.tabPageRecord.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // recordButton
+            // 
+            this.recordButton.Location = new System.Drawing.Point(86, 288);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(592, 59);
+            this.recordButton.TabIndex = 0;
+            this.recordButton.Text = "RECORD";
+            this.recordButton.UseVisualStyleBackColor = true;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(346, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.logInTab);
+            this.tabControl1.Controls.Add(this.tabPageRecord);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPageContacts);
+            this.tabControl1.Location = new System.Drawing.Point(-5, -1);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(774, 487);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // logInTab
+            // 
+            this.logInTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logInTab.BackgroundImage")));
+            this.logInTab.Controls.Add(this.label3);
+            this.logInTab.Controls.Add(this.saveButton);
+            this.logInTab.Controls.Add(this.userNameTexBox);
+            this.logInTab.Location = new System.Drawing.Point(4, 22);
+            this.logInTab.Name = "logInTab";
+            this.logInTab.Size = new System.Drawing.Size(766, 461);
+            this.logInTab.TabIndex = 4;
+            this.logInTab.Text = "Log In";
+            this.logInTab.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(99, 162);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // userNameTexBox
+            // 
+            this.userNameTexBox.Location = new System.Drawing.Point(13, 117);
+            this.userNameTexBox.Name = "userNameTexBox";
+            this.userNameTexBox.Size = new System.Drawing.Size(161, 20);
+            this.userNameTexBox.TabIndex = 0;
+            // 
+            // tabPageRecord
+            // 
+            this.tabPageRecord.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPageRecord.BackgroundImage")));
+            this.tabPageRecord.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPageRecord.Controls.Add(this.label4);
+            this.tabPageRecord.Controls.Add(this.recordButton);
+            this.tabPageRecord.Controls.Add(this.button2);
+            this.tabPageRecord.Location = new System.Drawing.Point(4, 22);
+            this.tabPageRecord.Name = "tabPageRecord";
+            this.tabPageRecord.Size = new System.Drawing.Size(766, 461);
+            this.tabPageRecord.TabIndex = 0;
+            this.tabPageRecord.Text = "Record";
+            this.tabPageRecord.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage2.BackgroundImage")));
+            this.tabPage2.Controls.Add(this.loadRecordButton);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(766, 461);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "My records";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // loadRecordButton
+            // 
+            this.loadRecordButton.Location = new System.Drawing.Point(451, 209);
+            this.loadRecordButton.Name = "loadRecordButton";
+            this.loadRecordButton.Size = new System.Drawing.Size(109, 23);
+            this.loadRecordButton.TabIndex = 1;
+            this.loadRecordButton.Text = "Load your record";
+            this.loadRecordButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 402);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 120);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "list\r\nlist\r\nlist\r\nlist\r\nlist\r\nlist\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My records";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(766, 461);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "How to use";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPageContacts
+            // 
+            this.tabPageContacts.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPageContacts.BackgroundImage")));
+            this.tabPageContacts.Location = new System.Drawing.Point(4, 22);
+            this.tabPageContacts.Name = "tabPageContacts";
+            this.tabPageContacts.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageContacts.Size = new System.Drawing.Size(766, 461);
+            this.tabPageContacts.TabIndex = 1;
+            this.tabPageContacts.Text = "Contact";
+            this.tabPageContacts.UseVisualStyleBackColor = true;
+            this.tabPageContacts.Click += new System.EventHandler(this.tabPageContacts_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(19, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Username";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(129, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "DateTime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Turquoise;
+            this.label4.Location = new System.Drawing.Point(262, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Start your record as %username%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(763, 441);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.97D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.logInTab.ResumeLayout(false);
+            this.logInTab.PerformLayout();
+            this.tabPageRecord.ResumeLayout(false);
+            this.tabPageRecord.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button recordButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageRecord;
+        private System.Windows.Forms.TabPage tabPageContacts;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button loadRecordButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage logInTab;
+        private System.Windows.Forms.TextBox userNameTexBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
